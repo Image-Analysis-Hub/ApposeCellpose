7b230b60e83184b960fdc90791f4c3afe404d6c3
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-05-27 14:41:49 +0200

    Use executable mvn wrapper instead of bash function

--- .github/build.sh
+++ .github/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Download build script`
+# Create a wrapper script that shadows the real mvn
+mkdir -p /tmp/mvn-bin
+cat > /tmp/mvn-bin/mvn << 'MVNWRAPPER'
+#!/bin/bash
+exec $(which -a mvn | tail -1) -DskipTests "$@"
+MVNWRAPPER
+chmod +x /tmp/mvn-bin/mvn
+
+# Put our wrapper first in PATH
+export PATH="/tmp/mvn-bin:$PATH"
+
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-build.sh
+bash ci-build.sh
 
-# Override mvn function to add -DskipTests
-mvn() {
-  command mvn -DskipTests "$@"
-}
-export -f mvn
-
-# Run build
-sh ci-build.sh
