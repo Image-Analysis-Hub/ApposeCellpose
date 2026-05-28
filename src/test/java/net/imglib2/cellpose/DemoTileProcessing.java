2d4a490fe4b8162aca7e63e24e85c7701497744d
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-05-28 19:15:39 +0200

    First working version of the tile processing demo.
    Just uses imglib2-ij, not the BDV.

--- src/test/java/net/imglib2/cellpose/DemoTileProcessing.java
+++ src/test/java/net/imglib2/cellpose/DemoTileProcessing.java
@@ -1,5 +1,6 @@
 package net.imglib2.cellpose;
 
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,10 +10,12 @@ import java.util.List;
 import org.apposed.appose.BuildException;
 import org.apposed.appose.TaskException;
 
-import bdv.util.BdvFunctions;
 import ij.IJ;
+import ij.ImageJ;
 import ij.ImagePlus;
 import ij.gui.NewImage;
+import ij.plugin.RGBStackMerge;
+import ij.process.ImageProcessor;
 import ij.process.LUT;
 import net.imglib2.Cursor;
 import net.imglib2.FinalDimensions;
@@ -25,7 +28,6 @@ import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.util.ImgUtil;
-import net.imglib2.util.Util;
 import net.imglib2.view.fluent.RandomAccessibleIntervalView;
 
 public class DemoTileProcessing
@@ -34,98 +36,140 @@ public class DemoTileProcessing
 	{
 		try
 		{
-
-//			ImageJ.main( args );
+			ImageJ.main( args );
 
 			// Input image.
 			final String filePath = "samples/IDR-1.tif";
 			final ImagePlus imp = IJ.openImage( filePath );
-//		imp.show();
 			final Img< T > img = ImageJFunctions.wrap( imp );
-			BdvFunctions.show( img, filePath );
-			final AxisInfo axisInfo = AxisInfo.XY;
 
+			// Output ImagePlus.
+			final ImagePlus outputImp = NewImage.createShortImage( "Cellpose output", ( int ) img.dimension( 0 ), ( int ) img.dimension( 1 ), 1, NewImage.FILL_BLACK );
 
-			// Output image.
-			final Img< UnsignedShortType > output = Util.getArrayOrCellImgFactory( img, new UnsignedShortType() ).create( img );
+			// Merge channels and set LUT.
+			final ImagePlus merged = RGBStackMerge.mergeChannels( new ImagePlus[] { imp, outputImp }, false );
+			merged.setTitle( "Cellpose Tile Processing Demo" );
+			merged.show();
+			merged.setC( 1 );
+			useLUT( merged.getChannelProcessor(), LUT.createLutFromColor( Color.WHITE ) );
+			merged.setC( 2 );
+			useGlasbeyDarkLUT( merged.getChannelProcessor() );
 
 			// Cellpose config.
-			final Cellpose3Parameters params = Cellpose3Parameters.defaultNucleiParameters();
+			final Cellpose3Parameters params = Cellpose3Parameters.builder()
+					.model( Cellpose3BuiltinModels.NUCLEI )
+					.diameter( 60.0 )
+					.resample( true )
+					.build();
 
 			// Tiles.
 			final int[] blockSize = new int[] { 512, 512 };
 			final FinalDimensions blockDims = new FinalDimensions( blockSize );
 			final List< Interval > chunks = Grids.collectAllContainedIntervals( img.dimensionsAsLongArray(), blockSize );
 
-			// Placeholders for tile processing.
-			final ShmImg< T > inputShmImg = Cellpose.createInputShmImg( blockDims, img.getType() );
-			final ShmImg< UnsignedShortType > outputLabelsShmImg = Cellpose.createOutputLabelsShmImg( blockDims, axisInfo, new UnsignedShortType() );
-
-			// Runner.
-//		final ApposeTaskListener listener = ApposeTaskListener.VOID;
-			final ApposeTaskListener listener = ApposeTaskListener.STD;
-
-			
-			final ImagePlus outputImp = NewImage.createShortImage( "Cellpose output", img.dimension( 0 ), img.dimension( 1 ), 1, NewImage.FILL_BLACK );
-			useGlasbeyDarkLUT( outputImp );
-
-			try (final CellposeRunner< T, UnsignedShortType > runner = Cellpose.cellpose3Runner(
-					params,
-					listener,
-					inputShmImg,
-					axisInfo,
-					outputLabelsShmImg,
-					null ))
+			// Process.
+			final long start = System.currentTimeMillis();
+			final AxisInfo axisInfo = AxisInfo.XY;
+			// Try-with-resources with auto-closeable ShmImgs and CellposeRunner.
+			try (
+					// Placeholders for tile processing.
+					final ShmImg< T > cellposeInputData = Cellpose.createInputShmImg( blockDims, img.getType() );
+					final ShmImg< UnsignedShortType > cellposeOutputData = Cellpose.createOutputLabelsShmImg( blockDims, axisInfo, new UnsignedShortType() );
+					// The runner.
+					final CellposeRunner< T, UnsignedShortType > runner = Cellpose.cellpose3Runner(
+							params,
+							ApposeTaskListener.VOID,
+							cellposeInputData,
+							axisInfo,
+							cellposeOutputData,
+							null );)
 			{
 				// Init runner.
 				runner.init();
 
 				// Process tiles.
-				int n = 1;
 				for ( final Interval tileInterval : chunks )
 				{
-					if ( n++ > 10 )
-						break; // DEBUG
+					// Input tile -> Cellpose input data location.
+					copyInput( img, cellposeInputData, tileInterval );
 
-					System.out.println( "\nProcessing tile: " + tileInterval );
-
-					System.out.println( "Copying input tile to shared memory..." );
-					final RandomAccessibleIntervalView< T > viewInput = img.view()
-							.interval( tileInterval )
-							.zeroMin();
-					ImgUtil.copy( viewInput, inputShmImg );
-
-					System.out.println( "Running Cellpose..." );
+					// Run Cellpose.
 					runner.run();
 
-					System.out.println( "Copying output labels from shared memory..." );
-					final RandomAccessibleIntervalView< UnsignedShortType > viewOutput = output.view()
-							.interval( tileInterval )
-							.zeroMin();
-
-					// Pedestrian copy to the output ImagePlus.
-					final Cursor< UnsignedShortType > c = outputLabelsShmImg.localizingCursor();
-					while ( c.hasNext() )
-					{
-						c.fwd();
-						final int x = ( int ) ( c.getIntPosition( 0 ) + tileInterval.min( 0 ) );
-						final int y = ( int ) ( c.getIntPosition( 1 ) + tileInterval.min( 1 ) );
-						final int val = c.get().get();
-						outputImp.getProcessor().set( x, y, val );
-					}
-					System.out.println( "Tile done." );
-
-					outputImp.resetDisplayRange();
-					outputImp.show();
+					// Cellpose output tile -> output ImagePlus.
+					copyOutput( cellposeOutputData, merged, tileInterval );
+					merged.resetDisplayRange();
+					merged.updateAndDraw();
 				}
-				System.out.println( "Done." );
+			}
+			finally
+			{
+				final long end = System.currentTimeMillis();
+				System.out.println( String.format( "Done in: %.2f seconds", ( end - start ) / 1000. ) );
 			}
 		}
 		catch ( final Exception e )
 		{
 			e.printStackTrace();
 		}
+	}
 
+	/**
+	 * Copy the Cellpose output labels contained in the tile interval to the
+	 * output ImagePlus. The Cellpose output ShmImg is supposed to be at origin
+	 * (0, 0) and of size equal to the tile size.
+	 * 
+	 * @param output
+	 *            the Cellpose output ShmImg containing the labels for the tile
+	 * @param target
+	 *            the output ImagePlus where to copy the labels
+	 * @param interval
+	 *            the tile interval defining the location of the tile in the
+	 *            output ImagePlus
+	 */
+	private static void copyOutput( final ShmImg< UnsignedShortType > output, final ImagePlus target, final Interval interval )
+	{
+		// Basic copy to the output ImagePlus.
+		final Cursor< UnsignedShortType > c = output.localizingCursor();
+		while ( c.hasNext() )
+		{
+			c.fwd();
+			final int x = ( int ) ( c.getIntPosition( 0 ) + interval.min( 0 ) );
+			if ( x >= target.getWidth() )
+				continue;
+			final int y = ( int ) ( c.getIntPosition( 1 ) + interval.min( 1 ) );
+			if ( y >= target.getHeight() )
+				continue;
+			final int val = c.get().get();
+			target.getProcessor().set( x, y, val );
+		}
+	}
+
+	/**
+	 * Copy the data of the input image contained in the tile interval to the
+	 * Cellpose input ShmImg. The Cellpose image is supposed to be at origin (0,
+	 * 0) and of size equal to the tile size. The input image must be defined
+	 * over all the tile interval.
+	 * 
+	 * @param <T>
+	 *            the pixel type
+	 * @param input
+	 *            the input image
+	 * @param target
+	 *            the Cellpose input ShmImg
+	 * @param interval
+	 *            the tile interval
+	 */
+	private static < T extends RealType< T > & NativeType< T > > void copyInput( final Img< T > input, final ShmImg< T > target, final Interval interval )
+	{
+		final RandomAccessibleIntervalView< T > viewInput = input.view()
+				.interval( interval )
+				.zeroMin();
+		final RandomAccessibleIntervalView< T > viewInputShmImg = target.view()
+				.translate( interval.minAsLongArray() )
+				.interval( interval )
+				.zeroMin();
+		ImgUtil.copy( viewInput, viewInputShmImg );
 	}
 
 	private static LUT loadLutFromResource( final String resourcePath )
@@ -178,16 +222,15 @@ public class DemoTileProcessing
 		}
 	}
 
-	public static final void useGlasbeyDarkLUT( final ImagePlus imp )
+	private static final void useGlasbeyDarkLUT( final ImageProcessor ip )
 	{
 		final LUT lut = loadLutFromResource( "/glasbey_on_dark.lut" );
-		useLUT( imp, lut );
+		useLUT( ip, lut );
 	}
 
-	public static final void useLUT( final ImagePlus imp, final LUT lut )
+	private static final void useLUT( final ImageProcessor ip, final LUT lut )
 	{
-		imp.setLut( lut );
-		imp.updateAndDraw();
+		ip.setLut( lut );
 	}
 
 }
