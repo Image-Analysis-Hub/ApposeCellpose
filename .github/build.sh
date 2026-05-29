614d6247699225231b7bb237a42820d372db69b1
Stephane Rigaud <Stephane.rigaud@pasteur.fr>
2026-05-29 16:51:23 +0200

    skip tests build-arg
    Remove mvn wrapper script and update build command.

--- .github/build.sh
+++ .github/build.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-# Create a wrapper script that shadows the real mvn
-mkdir -p /tmp/mvn-bin
-cat > /tmp/mvn-bin/mvn << 'MVNWRAPPER'
-#!/bin/bash
-exec $(which -a mvn | tail -1) -DskipTests "$@"
-MVNWRAPPER
-chmod +x /tmp/mvn-bin/mvn
-
-# Put our wrapper first in PATH
-export PATH="/tmp/mvn-bin:$PATH"
-
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-build.sh
-bash ci-build.sh
+BUILD_ARGS=-DskipTests sh ci-build.sh
+
 
